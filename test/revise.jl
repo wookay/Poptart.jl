5b5a9f74996ca58ca8cb4b34f73a1f6ae54d4adb
WooKyoung Noh <wookay.noh@gmail.com>
2019-02-01 11:54:53 +0900

    check close_window_before_revise

--- test/revise.jl
+++ test/revise.jl
@@ -1,7 +1,15 @@
 using Revise, Jive
 using Poptart
+using GLFW
+
+close_window_before_revise = false
+
 watch(@__DIR__, sources=[pathof(Poptart)]) do path
     @info :changed path
+    if close_window_before_revise
+        win = GLFW.GetCurrentContext()
+        win.handle !== C_NULL && ccall((:glfwSetWindowShouldClose, GLFW.lib), Cvoid, (GLFW.Window, Cint), win, 189)
+    end
     revise()
     runtests(@__DIR__, targets=ARGS, skip=["revise.jl"])
 end
