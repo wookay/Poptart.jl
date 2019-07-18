6545f36c0a123a22bc1bfe0eedb79ce5b677674c
WooKyoung Noh <wookay.noh@gmail.com>
2019-07-18 23:56:00 +0900

    travis xvfb

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 ENV["POPTART_AUTO_CLOSE"] = true
 
 using Jive
-runtests(@__DIR__, skip=["revise.jl"])
+travis_skip = haskey(ENV, "TRAVIS") && ENV["TRAVIS_OS_NAME"] == "osx" ? ["cimgui", "poptart/desktop"] : []
+runtests(@__DIR__, skip=["revise.jl", travis_skip...])
